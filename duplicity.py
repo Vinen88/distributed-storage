5588b9ebb89a53bf1a1b6753ddbadeb0eccf58cb
Jason Robock <jrobock@ualberta.ca>
2023-09-04 17:40:58 -0600

    configs/updates

--- duplicity.py
+++ duplicity.py
@@ -1,20 +1,52 @@
 import os
+import sys
+from operator import itemgetter
 from subprocess import Popen, PIPE
 from dotenv import load_dotenv
+import tomllib
+from typing import Union, Literal
+from functools import partial
 
 load_dotenv()
 
-
-def dupe_backup(dir: str, url: str):
-    if os.path.isdir(dir):
-        p = Popen(["duplicity", "bu", dir, url], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        print(p)
-        # output = p.communicate(input=pf)[0].decode()
-        # print("wioefjiowejfiowjef", output)
-    else:
-        print("Not a directory")
+Config = dict[Union[Literal["peers"], Literal["backup"]]]
+eprint = partial(print, file=sys.stderr)
 
 
-if __name__ == "__main__":
-    url = os.getenv("DUPE_URL")
-    dupe_backup("test_dir", url)
+class Duplicity:
+    def __init__(self, filepath) -> None:
+        config = Duplicity.load_config(filepath)
+        self.peers = Duplicity.get_some(config["peers"], "url")
+        self.dirs = Duplicity.get_some(config["backups"], "dir")
+
+    @staticmethod
+    def load_config(filepath: str) -> Config:
+        with open(filepath, "rb") as f:
+            return tomllib.load(f)
+
+    @staticmethod
+    def get_some(config: dict, key: str) -> list:
+        return tuple(map(itemgetter(key), config))
+
+    def dupe_backup(self) -> None:
+        for peer in self.peers:
+            # ping peer
+            for directory in self.dirs:
+                if os.path.isdir(directory):
+                    Popen(
+                        ["duplicity", "bu", directory, peer],
+                        stdin=PIPE,
+                        stdout=PIPE,
+                        stderr=PIPE,
+                    )
+                else:
+                    eprint(f"{directory} is not a valid directory.")
+
+
+def main():
+    duper = Duplicity("config.toml")
+    duper.dupe_backup()
+
+
+if __name__ == "__main__" and not sys.flags.interactive:
+    main()
